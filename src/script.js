55b1d54294f1060203a42dc803b4c2f6be0a90ff
unknown <nesad.omeragich27@gmail.com>
2020-03-19 18:49:19 +0100

    Changed fetched data destructuring

--- src/script.js
+++ src/script.js
@@ -9,39 +9,39 @@ function createNode(el) {
     return document.createElement(el);
 }
 
-
-//console.log(data.data.children[0].data.title)
 function getData() {
     fetch('https://www.reddit.com/r/popular.json')
     .then(resp => resp.json())
-    .then(data => {
+    .then(({data}) => {
+
+        const {children: postList} = data;
+
         let posts = document.createElement('ul');
-        
-        data.data.children.forEach(post => {
+
+        postList.forEach(post => {
 
             let newPost = createNode('li');
 
-            let postLink = createNode('a');
-            
+            let postLink = createNode('a');         
             postLink.href = post.data.url;
             postLink.target = "_blank";
 
             let title = createNode('p');
             title.textContent = post.data.title;
+            append(postLink, title);
 
             let score = createNode('span');
             score.textContent = `${post.data.score} pts`;
+            append(postLink, score);
 
             let author = createNode('span');
-            author.textContent = `by ${post.data.author}`
+            author.textContent = `by ${post.data.author}`;
+            append(postLink, author);
 
             let comments_num = createNode('span');
-            comments_num.textContent = `${post.data.num_comments} comments`
-
-            append(postLink, title);
-            append(postLink, score);
-            append(postLink, author);
+            comments_num.textContent = `${post.data.num_comments} comments`;
             append(postLink, comments_num);
+
             append(newPost, postLink);
             append(posts, newPost)      
         })
