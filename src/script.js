d102af897d8de3b941a1d148ccd09602e27821b3
unknown <nesad.omeragich27@gmail.com>
2020-03-20 20:17:30 +0100

    Changed fetching logic

--- src/script.js
+++ src/script.js
@@ -1,12 +1,7 @@
-
 let output = document.getElementById('output');
 
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-
-function createNode(el) {
-    return document.createElement(el);
+function returnPost(link, title, score, author, comments_num) {
+    return `<a href='${link}' target='_blank'><li><p>${title}</p><span>${score} pts</span><span>by ${author}</span><span>${comments_num} comments</span></li></a>`;
 }
 
 function getData() {
@@ -14,37 +9,21 @@ function getData() {
     .then(resp => resp.json())
     .then(({data}) => {
 
-        const {children: postList} = data;
+        const postList = data.children;
 
-        let posts = document.createElement('ul');
+        const posts = document.createElement('ul');
 
         postList.forEach(post => {
-
-            let newPost = createNode('li');
-
-            let postLink = createNode('a');         
-            postLink.href = post.data.url;
-            postLink.target = "_blank";
-
-            let title = createNode('p');
-            title.textContent = post.data.title;
-            append(postLink, title);
-
-            let score = createNode('span');
-            score.textContent = `${post.data.score} pts`;
-            append(postLink, score);
-
-            let author = createNode('span');
-            author.textContent = `by ${post.data.author}`;
-            append(postLink, author);
-
-            let comments_num = createNode('span');
-            comments_num.textContent = `${post.data.num_comments} comments`;
-            append(postLink, comments_num);
-
-            append(newPost, postLink);
-            append(posts, newPost)      
-        })
-        append(output, posts)
+            posts.insertAdjacentHTML('beforeend', returnPost(
+                post.data.url,
+                post.data.title,
+                post.data.score,
+                post.data.author,
+                post.data.num_comments
+            ));   
+        });
+        output.appendChild(posts); 
     })
+     .catch((err) => console.log('Error: ', err));  
 }
+getData();
